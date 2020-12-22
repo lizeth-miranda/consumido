6bcb1aca748eee9985317b8116031f14e83963b0
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2020-12-22 13:52:33 -0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Material Consumido',
+    'version': '13.1',
+    'author': 'Demsa',
+    'website': '',
+    'depends': [
+        'mrp',
+
+
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/mrp_production.xml',
+        'views/stock_picking.xml',
+
+    ],
+}
